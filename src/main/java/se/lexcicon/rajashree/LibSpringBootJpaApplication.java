208ce4a67869cd8b3faed9d34761e6d4a2237433
Rajashree <bhuyan.rajashree@gmail.com>
2022-06-19 23:26:51 +0200

    updated with part1 and part2 and part3

--- src/main/java/se/lexcicon/rajashree/LibSpringBootJpaApplication.java
+++ src/main/java/se/lexcicon/rajashree/LibSpringBootJpaApplication.java
@@ -2,12 +2,21 @@ package se.lexcicon.rajashree;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.metrics.ApplicationStartup;
 
 @SpringBootApplication
-public class LibSpringBootJpaApplication {
+public class LibSpringBootJpaApplication implements CommandLineRunner {
+
+
 
     public static void main(String[] args) {
         SpringApplication.run(LibSpringBootJpaApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        System . out . println ( " Application Started !! " );
+    }
 }
